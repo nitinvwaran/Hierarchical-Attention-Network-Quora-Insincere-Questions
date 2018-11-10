c7442d8a29671cff64cfed1093c1393eb99d3e31
Nitin <nitin.vwaran@gmail.com>
2018-11-10 15:29:40 +0800

    undersampling

--- google_analytics_revenue/model_xgboost.py
+++ google_analytics_revenue/model_xgboost.py
@@ -15,25 +15,37 @@ def training():
     df_train = pd.read_csv(file_name)
     df_train.drop('Unnamed: 0',axis=1,inplace=True)
     y = df_train.loc[:, 'is_revenue']
-    df_train.drop('is_revenue',axis=1,inplace=True)
+    #df_train.drop('is_revenue',axis=1,inplace=True)
 
     n_folds = 10
     auc = []
 
-    rf = RandomForestClassifier(n_estimators=100,class_weight='balanced')
+    mini_batch_size = 10000
+
+    rf = RandomForestClassifier(n_estimators=100,class_weight='balanced',min_samples_split=10)
     for i in range(0,n_folds):
 
 
         X_train, X_dev, y_train, y_dev = train_test_split(df_train,y,stratify=y,test_size=0.1)
 
+        X_dev.drop('is_revenue',axis=1,inplace=True)
+        X_train_0 = X_train.loc[X_train['is_revenue'] == 0]
+        X_train_1 = X_train.loc[X_train['is_revenue'] != 0]
 
         print ('Fold:' + str(i))
         print (X_train.shape)
         print (X_dev.shape)
 
+        sample_0 = X_train_0.sample(n = int(round(mini_batch_size * 1)))
+        sample_1 = X_train_1.sample(n= int(round(mini_batch_size * 1)))
 
+        mini_batch_sample = pd.concat([sample_0,sample_1],ignore_index=True)
+        mini_batch = mini_batch_sample.sample(frac=1).reset_index(drop=True) # reshuffles
 
-        rf.fit(X_train,y_train)
+        y_mini_batch = mini_batch.loc[:,'is_revenue']
+        mini_batch.drop('is_revenue',axis=1,inplace = True)
+
+        rf.fit(mini_batch,y_mini_batch)
 
 
         y_prob = rf.predict_proba(X_dev)[:,1]
@@ -50,7 +62,7 @@ def training():
     print ('The average auc ')
     print (auc_avg)
 
-    fit_model_after_cv(df_train,y)
+    #fit_model_after_cv(df_train,y)
 
 def fit_model_after_cv(df_train, y):
 
